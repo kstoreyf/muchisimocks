4f327818e93ba22a3507a898b51831502930db2f
Kate S-F <kstoreyfisher@gmail.com>
2024-02-22 12:17:27 +0100

    refactored emu pk generation script

--- scripts/generate_emuPks.py
+++ scripts/generate_emuPks.py
@@ -7,29 +7,33 @@ import baccoemu
 def main():
 
     tag_emuPk = '_2param'
+    n_tot = 1000
 
-    param_bounds = {'omega_cold': [0.23, 0.4],
-                    'sigma8_cold': [0.73, 0.9],
-                    'hubble': [0.6, 0.8],
-                    'ns': [0.92, 1.01],
-                    'omega_baryon': [0.04, 0.06]}
+    #emu = baccoemu.Lbias_expansion(verbose=False)
+    fn_emu = '/dipc_storage/cosmosims/data_share/lbias_emulator/lbias_emulator2.0.0'
+    emu = baccoemu.Lbias_expansion(nonlinear_emu_path=fn_emu,
+                                nonlinear_emu_details='details.pickle',
+                                nonlinear_emu_field_name='NN_n',
+                                nonlinear_emu_read_rotation=False,
+                                verbose=False)
+    
     if '_2param' in tag_emuPk:
         param_names = ['omega_cold', 'sigma8_cold']
     elif '_5param' in tag_emuPk:
         param_names = ['omega_cold', 'sigma8_cold', 'hubble', 'ns', 'omega_baryon']
     else:
         raise KeyError('define parameters!')
-    n_params = len(param_names)
-    n_tot = 1000
     
-    sampler = qmc.LatinHypercube(d=n_params, seed=42)
-    theta_orig = sampler.random(n=n_tot)
+    param_keys = emu.emulator['nonlinear']['keys']
+    emu_bounds =  emu.emulator['nonlinear']['bounds']
+    param_bounds = {name: emu_bounds[param_keys.index(name)] for name in param_names}
+    
+    theta = latin_hypercube(param_names, param_bounds, n_tot)
+    generate_pks(theta, param_names, emu, tag_emuPk)
 
-    l_bounds = [param_bounds[param_name][0] for param_name in param_names]
-    u_bounds = [param_bounds[param_name][1] for param_name in param_names]
-    theta = qmc.scale(theta_orig, l_bounds, u_bounds)
-
-    emu = baccoemu.Lbias_expansion()
+    
+def generate_pks(theta, param_names, emu, tag_emuPk):
+    
     cosmo_params = setup_cosmo_emu()
     bias_params = [1., 0., 0., 0.]
     k = np.logspace(-2, np.log10(0.75), 30)
@@ -50,8 +54,18 @@ def main():
     header = ','.join(param_names)
     np.savetxt(fn_emuPk_params, theta, header=header, delimiter=',', fmt='%.8f')
     np.savetxt(fn_emuk, k)
+    return pks
            
+  
+def latin_hypercube(param_names, param_bounds, n_tot):
+    n_params = len(param_names)
+    sampler = qmc.LatinHypercube(d=n_params, seed=42)
+    theta_orig = sampler.random(n=n_tot)
 
+    l_bounds = [param_bounds[param_name][0] for param_name in param_names]
+    u_bounds = [param_bounds[param_name][1] for param_name in param_names]
+    theta = qmc.scale(theta_orig, l_bounds, u_bounds)
+    return theta
 
 
 def setup_cosmo_emu():
@@ -73,6 +87,8 @@ def setup_cosmo_emu():
         'expfactor'     :  1
     }
     return cosmo_params
+
+
 
 
 
