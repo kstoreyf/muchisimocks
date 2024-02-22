498385b4602552dbe0026041ddc22d81cd8a8ee9
Kate S-F <kstoreyfisher@gmail.com>
2024-02-22 12:17:50 +0100

    short script to compute gaussian noise for a given Pk with bacco approximate function

---
+++ scripts/compute_gaussian_noise.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+import bacco
+
+def main():
+    tag_emuPk = '_2param'
+    kk, Pk = load_emuPks(tag_emuPk)
+    box_size = 1000.
+    
+    fn_emuPkerrG = f'../data/emuPks/emuPks_errgaussian_{tag_emuPk}.npy'
+
+    errs_gaussian = []
+    for i in range(Pk.shape[0]):
+        print(i)
+        err_gaussian = bacco.statistics.approx_pk_gaussian_error(kk, Pk[i], box_size)
+        errs_gaussian.append(err_gaussian)
+    errs_gaussian = np.array(errs_gaussian)
+    np.save(fn_emuPkerrG, errs_gaussian)
+    
+    
+def load_emuPks(tag_emuPk):
+
+    fn_emuPk = f'../data/emuPks/emuPks{tag_emuPk}.npy'
+    fn_emuk = f'../data/emuPks/emuPks_k{tag_emuPk}.txt'
+
+    Pk = np.load(fn_emuPk)
+    #theta = np.genfromtxt(fn_emuPk_params, delimiter=',', names=True)
+    #param_names = theta.dtype.names
+    # from tuples to 2d array
+    #theta = np.array([list(tup) for tup in theta])
+    kk = np.genfromtxt(fn_emuk)
+    return kk, Pk
+
+
+if __name__=='__main__':
+    main()
