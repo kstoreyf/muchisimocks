fixed shame mocks to use average of phases

scripts/compute_statistics.py
@@ -562,17 +562,22 @@ def compute_bispectrum(base, tracer_field, fn_stat=None):
     bk_corr = bspec.Bk_ideal(tracer_field, discreteness_correction=True)
 
     if fn_stat is not None:
-        k123 = bspec.get_ks()
-        weight = k123.prod(axis=0)
-        bispec_results_dict = {
-            'k123': k123,
-            'bispectrum': bk_corr,
-            'weight': weight,
-        }
-        Path.absolute(Path(fn_stat).parent).mkdir(parents=True, exist_ok=True)
-        np.save(fn_stat, bispec_results_dict)
+        save_bispectrum(fn_stat, bspec, bk_corr)
             
     return bspec, bk_corr
+
+
+def save_bispectrum(fn_stat, bspec, bk_corr):
+    k123 = bspec.get_ks()
+    weight = k123.prod(axis=0)
+    bispec_results_dict = {
+        'k123': k123,
+        'bispectrum': bk_corr,
+        'weight': weight,
+    }
+    Path.absolute(Path(fn_stat).parent).mkdir(parents=True, exist_ok=True)
+    np.save(fn_stat, bispec_results_dict)
+    return
 
 
 if __name__ == '__main__':

scripts/data_loader.py
@@ -75,8 +75,8 @@ def load_data_shame(statistic, tag_mock):
     if statistic == 'pk':
         k, stat, error, pk_obj = load_pk(fn_statistics)
     elif statistic == 'bispec':
-        # easiest way to get n_grid, which is needed for load_bispec
-        fn_cat_mesh = f'../data/data_shame/data{tag_mock}/tracer_field.npy'
+        # easiest way to get n_grid, which is needed for load_bispec (just take one of the phases)
+        fn_cat_mesh = f'../data/data_shame/data{tag_mock}/tracer_field_phase0.npy'
         tracer_field = np.load(fn_cat_mesh, allow_pickle=True)
         n_grid_mock = tracer_field.shape[0]
         k, stat, error, bispec_obj = load_bispec(fn_statistics, n_grid_mock)
@@ -267,10 +267,7 @@ def load_params_ood(data_mode, tag_mock, dir_params='../data/params'):
     if data_mode == 'shame':
         # needed this line bc was getting error message
         import bacco
-        # TODO check that this was actually its params! just says Planck in filename
-        pdict = bacco.cosmo_parameters.Planck18
-        bacco.configuration.update({'growth':{'method': 'reps'}})
-        cosmo_mock = utils.get_cosmo(pdict)
+        cosmo_mock = utils.get_cosmo(utils.cosmo_dict_shame)
 
         param_dict = {}
         for k, v in cosmo_mock.pars.items():
@@ -278,6 +275,9 @@ def load_params_ood(data_mode, tag_mock, dir_params='../data/params'):
             if k=='sigma8':
                 kname = 'sigma8_cold'
             param_dict[kname] = v
+
+        param_dict.update({'b1': 0.47409821, 'b2': 0.06306578, 'bs2': -0.17022439, 'bl': -0.83432633}) #(via marcos)
+
         return param_dict
     else:
         raise ValueError(f"Data mode {data_mode} not recognized!")
@@ -455,12 +455,12 @@ def load_theta_test(tag_params_test, tag_biasparams_test, tag_Anoise_test=None,
 def load_theta_ood(data_mode, tag_mock,
                    cosmo_param_names_vary=None, bias_param_names_vary=None, noise_param_names_vary=None,):
     param_dict = load_params_ood(data_mode, tag_mock)
-    theta_test = [param_dict[pname] for pname in cosmo_param_names_vary]
-    theta_test.extend([np.nan for pname in bias_param_names_vary])
+    theta_test = [param_dict[pname] if pname in param_dict else np.nan for pname in cosmo_param_names_vary]
+    theta_test.extend([param_dict[pname] if pname in param_dict else np.nan for pname in bias_param_names_vary])
     if 'An1' in tag_mock:
         theta_test.extend([1])
     else:
-        theta_test.extend([np.nan for pname in noise_param_names_vary])
+        theta_test.extend([param_dict[pname] if pname in param_dict else np.nan for pname in noise_param_names_vary])
     return np.array(theta_test)
 
     

scripts/process_catalogs.py
@@ -23,12 +23,13 @@ def main():
     Main function to process SHAMe catalog and compare with muchisimocks data.
     """
     # Configuration
-    dir_cat = '../data/shame_catalogues_to_share'
-    fn_cat = f'{dir_cat}/kate_sham_catalogue_a1.0_par_b_Planck_N3072_L1024_0.00.h5'
+    dir_cat = '../data/shame_catalogues_to_share/kate'
+    fn_cat0 = f'{dir_cat}/kate_sham_catalogue_a1.0_par_b_Planck_N3072_L1024_0.00_0.00022.h5'
+    fn_catpi = f'{dir_cat}/kate_sham_catalogue_a1.0_par_b_Planck_N3072_L1024_3.14_0.00022.h5'
     data_mode = 'shame'
     tag_mock = '_An1'
     statistics = ['pk', 'bispec']
-    overwrite = False
+    overwrite = True
     
     # Grid and box parameters
     box_size_mock = 1024.0
@@ -36,11 +37,18 @@ def main():
     print("=== Processing SHAMe Catalog ===")
     
     # Process catalog to tracer_field field
-    fn_cat_mesh = f'../data/data_{data_mode}/data{tag_mock}/tracer_field.npy'
-    tracer_field, n_grid_mock = process_catalog_to_mesh(
-        fn_cat, box_size_mock, fn_cat_mesh=fn_cat_mesh, overwrite=overwrite
+    fn_cat0_mesh = f'../data/data_{data_mode}/data{tag_mock}/tracer_field_phase0.npy'
+    tracer_field_0, n_grid_mock_0 = process_catalog_to_mesh(
+        fn_cat0, box_size_mock, fn_cat_mesh=fn_cat0_mesh, overwrite=overwrite
+    )
+    fn_catpi_mesh = f'../data/data_{data_mode}/data{tag_mock}/tracer_field_phasepi.npy'
+    tracer_field_pi, n_grid_mock_pi = process_catalog_to_mesh(
+        fn_catpi, box_size_mock, fn_cat_mesh=fn_catpi_mesh, overwrite=overwrite
     )
     
+    #cosmo = utils.get_cosmo(utils.cosmo_dict_shame)
+    cosmo = None #feel like should be assuming i don't know cosmo?
+
     for statistic in statistics:
         dir_statistics = f'../data/data_{data_mode}/data{tag_mock}/{statistic}s'
         fn_stat = f'{dir_statistics}/{statistic}.npy'
@@ -50,22 +58,33 @@ def main():
         
         if statistic=='pk':
             print("\n=== Computing Power Spectrum ===")
-            compute_pk(tracer_field, box_size_mock,
-                                 fn_stat=fn_stat)
+            pk_0 = compute_pk(tracer_field_0, box_size_mock, cosmo=cosmo)
+            pk_pi = compute_pk(tracer_field_pi, box_size_mock, cosmo=cosmo)
+            pk_mean = {} #for now ignoring any other entries in the dict, haven't been using
+            pk_mean['k'] = pk_0['k'] 
+            pk_mean['pk'] = 0.5 * (pk_0['pk'] + pk_pi['pk'])
+            pk_mean['pk_gaussian_error'] = 0.5 * (pk_0['pk_gaussian_error'] + pk_pi['pk_gaussian_error']) #??
+            Path.absolute(Path(fn_stat).parent).mkdir(parents=True, exist_ok=True)
+            np.save(fn_stat, pk_mean)
             print(f"Power spectrum saved to {fn_stat}")
         
         elif statistic=='bispec':
             print("\n=== Computing Bispectrum ===")
-            compute_bispectrum(
-                tracer_field, box_size_mock, n_grid_mock,
-                fn_stat=fn_stat
-            )
+            bspec_0, bk_corr_0 = compute_bispectrum(
+                tracer_field_0, box_size_mock, n_grid_mock_0)
+            bspec_pi, bk_corr_pi = compute_bispectrum(
+                tracer_field_pi, box_size_mock, n_grid_mock_pi)
+            
+            bspec_mean = bspec_0  # Both should have same k-bins
+            bk_corr_mean = {} #for now ignoring any other entries in the dict, haven't been using
+            bk_corr_mean['b0'] = 0.5 * (bk_corr_0['b0'] + bk_corr_pi['b0'])
+            cs.save_bispectrum(fn_stat, bspec_mean, bk_corr_mean)
             print(f"Bispectrum saved to {fn_stat}")
 
         else:
             raise ValueError(f"Statistic {statistic} not recognized!")
         
-    print(f"Finished processing SHAMe catalog {fn_cat}")
+    print(f"Finished processing SHAMe catalog {fn_cat0} (and its pi partner)")
     
 
 def round_to_nearest_even(x):
@@ -108,6 +127,7 @@ def remove_highk_modes(field, box_size_mock, n_grid_target):
 
 def process_catalog_to_mesh(fn_cat, box_size_mock, fn_cat_mesh=None, 
                             n_grid_target=128, n_grid_orig=512, box_size_muchisimocks=1000.0, 
+                            cosmo=None,
                             overwrite=False):
     """
     Process a catalog file to create density mesh and tracer_field field.
@@ -160,7 +180,7 @@ def process_catalog_to_mesh(fn_cat, box_size_mock, fn_cat_mesh=None,
         interlacing=False, 
         deposit_method='cic',
         zspace=False, 
-        cosmology=None
+        cosmology=cosmo
     )
     print(f"High-res mesh shape: {cat_mesh_ngorig.shape}")
     

scripts/utils.py
@@ -73,6 +73,23 @@ cosmo_dict_quijote = {
                 'tau'           :  0.0952,
                 }   
 
+cosmo_dict_shame = {
+            'omega_cdm'     : 0.2603,
+            'omega_baryon'  : 0.0486,
+            'omega_cold'    : 0.30889999999999995,
+            'omega_m'       : 0.30889999999999995,
+            'neutrino_mass' : 0.0,
+            'hubble'        : 0.6774,
+            'ns'            : 0.9667,
+            'As'            : 2.0669036404058968e-09,
+            'sigma8_cold'   : 0.8159,
+            'sigma8'        : 0.8159000277519226,
+            'w0'            : -1.0,
+            'wa'            : 0.0,
+            'tau'           : 0.0952,
+}
+
+
 cosmo_param_names_ordered = ['omega_cold', 'sigma8_cold', 'hubble', 'omega_baryon', 'ns']
 biasparam_names_ordered = ['b1', 'b2', 'bs2', 'bl'] 
 param_names_all_ordered = cosmo_param_names_ordered + biasparam_names_ordered
@@ -329,10 +346,11 @@ def param_dict_to_bacco_param_dict(param_dict, neutrino_mass=None):
         else:
             name_bacco = name_orig
         param_dict_bacco[name_bacco] = param_dict[name_orig] 
-        
-    pn_oms = ['omega_m', 'omega_cdm', 'omega_cold']
-    n_oms = np.sum([1 if pn_om in param_dict_bacco else 0 for pn_om in pn_oms])        
-    assert n_oms==1, f"should pass exactly one of {pn_oms}!"     
+
+    # i think it's fine to just take them in this order?        
+    # pn_oms = ['omega_m', 'omega_cdm', 'omega_cold']
+    # n_oms = np.sum([1 if pn_om in param_dict_bacco else 0 for pn_om in pn_oms])        
+    # assert n_oms==1, f"should pass exactly one of {pn_oms}!"     
               
     if 'omega_cdm' in param_dict_bacco:
         pass      